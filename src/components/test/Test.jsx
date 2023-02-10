6ad667ea5e2fc6cedbb846e8c0d4031f5132272b
GerganaNoneva <gergananoneva@gmail.com>
2023-02-10 18:42:21 +0200

    Add Logo Component

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -3,11 +3,12 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Test.module.scss";
 import classNames from "classnames";
+import Logo from "../logo/Logo"
 
 export default function Test() {
   return (
     <div className={classNames(styles.wrapper)}>
-      
+      <Logo type='muted'/>
     </div>
   );
 }
