1a04c8998a8903880b5db115717319c2851658b4
GerganaNoneva <gergananoneva@gmail.com>
2023-02-10 18:36:02 +0200

    Start Project

---
+++ src/components/test/Test.jsx
@@ -0,0 +1,13 @@
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import Link from "../link/Link";
+import styles from "./Test.module.scss";
+import classNames from "classnames";
+
+export default function Test() {
+  return (
+    <div className={classNames(styles.wrapper)}>
+      
+    </div>
+  );
+}
