50c904130c90a97f034e8df090b90488a1fb3e49
GerganaNoneva <gergananoneva@gmail.com>
2023-02-10 22:39:01 +0200

    Add Avatar Component

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -5,11 +5,12 @@ import styles from "./Test.module.scss";
 import classNames from "classnames";
 import Logo from "../logo/Logo"
 import Header from "../header/Header"
+import Avatar from "../avatar/Avatar"
 
 export default function Test() {
   return (
     <div className={classNames(styles.wrapper)}>
-      <Header/>
+      <Avatar verified url='/images/avatar.png'/>
     </div>
   );
 }
