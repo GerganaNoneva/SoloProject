4604085af189cf55ee7b462b80c457dbe802dbde
GerganaNoneva <gergananoneva@gmail.com>
2023-02-11 02:30:36 +0200

    Add Trending Component

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -9,58 +9,73 @@ import Avatar from "../avatar/Avatar"
 import User from "../user/User"
 import NftCard from "../card/Card"
 import Trending from "../trending/Trending"
+/*
+const cards = [
+   {
+      "name":"Ivy",
+      "user":{
+         "avatar":{
+            "url":"images/avatar.png"
+         },
+         "verified":true
+      },
+      "mediaUrl":"images/nft.jpg",
+      "price":1,
+      "currency":"ETH"
+   },
+   {
+      "name":"Judie",
+      "user":{
+         "avatar":{
+            "url":"images/avatar.png"
+         },
+         "verified":true
+      },
+      "mediaUrl":"images/nft.jpg",
+      "price":2.3,
+      "currency":"ETH"
+   },
+   {
+      "name":"Juniper",
+      "user":{
+         "avatar":{
+            "url":"images/avatar.png"
+         },
+         "verified":true
+      },
+      "mediaUrl":"images/nft.jpg",
+      "price":5,
+      "currency":"ETH"
+   },
+   {
+      "name":"Maple",
+      "user":{
+         "avatar":{
+            "url":"images/avatar.png"
+         },
+         "verified":true
+      },
+      "mediaUrl":"images/nft.jpg",
+      "price":10,
+      "currency":"ETH"
+   }
+];*/
 
-const cardsInfo=[
-  {
-     "name":"Ivy",
-     "user":{
-        "avatar":{
-           "url":"images/avatar.png"
-        },
-        "verified":true
+const cards=[{
+   name: 'Clock',
+   likes:1000000,
+   mediaUrl: '/images/nft.jpg',
+   user: {
+     avatar: {
+       url: '/images/avatar.png'
      },
-     "mediaUrl":"images/nft.jpg",
-     "price":1,
-     "currency":"ETH"
-  },
-  {
-     "name":"Judie",
-     "user":{
-        "avatar":{
-           "url":"images/avatar.png"
-        },
-        "verified":true
-     },
-     "mediaUrl":"images/nft.jpg",
-     "price":2.3,
-     "currency":"ETH"
-  },
-  {
-     "name":"Juniper",
-     "user":{
-        "avatar":{
-           "url":"images/avatar.png"
-        },
-        "verified":true
-     },
-     "mediaUrl":"images/nft.jpg",
-     "price":5,
-     "currency":"ETH"
-  },
-  {
-     "name":"Maple",
-     "user":{
-        "avatar":{
-           "url":"images/avatar.png"
-        },
-        "verified":true
-     },
-     "mediaUrl":"images/nft.jpg",
-     "price":10,
-     "currency":"ETH"
-  }
-];
+     verified: true
+   },
+   price: '12.2',
+   currency: 'ETH'
+ }]
 
+/*
 const nft = {
   name: 'Clock',
   likes:1000000,
@@ -74,18 +89,18 @@ const nft = {
   price: '12.2',
   currency: 'ETH'
 };
-
+*/
 
 export default function Test() {
   return (
     <div className={classNames(styles.wrapper)}>
-      <NftCard {...nft}/>
-     
+      <Trending cards={cards}/>
     </div>
   );
 }
 
 // <Trending cards={cardsInfo}/>
+//      <NftCard {...nft}/>
 
 
 
